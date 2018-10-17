be52e1e92b18016fe8985cf94d94611dc1239750
Angello Obel <angello@obel.co>
2018-10-17 20:40:34 +0300

    shows a checkmark if selected

--- src/components/isp-select/isp/index.js
+++ src/components/isp-select/isp/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import CheckIcon from '@material-ui/icons/Check';
 
-const ISP = ({ isp, onSelect }) => (
-  <ListItem onClick={onSelect} button>
-    <ListItemText primary={isp} />
+const ISP = ({ isp, onSelect, selected }) => (
+  <ListItem
+    onClick={onSelect}
+    button
+    disableGutters
+  >
+    <ListItemText style={{ paddingLeft: 4 }} primary={isp} />
+    {selected && (
+    <ListItemIcon>
+      <CheckIcon />
+    </ListItemIcon>
+    )}
   </ListItem>
 );
 
 ISP.propTypes = {
   isp: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
 };
 
 export default ISP;
--- src/components/isp-select/isp/test.js
+++ src/components/isp-select/isp/test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import mount from 'enzyme/mount';
 import ListItem from '@material-ui/core/ListItem';
+import CheckIcon from '@material-ui/icons/Check';
 import ISP from '.';
 
 const mockPlans = [];
@@ -8,7 +9,12 @@ const mockPlans = [];
 describe('ISP', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(<ISP isp="ISP name" plans={mockPlans} onSelect={jest.fn()} />);
+    wrapper = mount(<ISP
+      isp="ISP name"
+      plans={mockPlans}
+      selected={false}
+      onSelect={jest.fn()}
+    />);
   });
   it('should render', () => {
     expect(wrapper.exists()).toBeTruthy();
@@ -19,5 +25,14 @@ describe('ISP', () => {
   it('shoud contain a list item that calls onSelect when clicked', () => {
     wrapper.find(ListItem).simulate('click');
     expect(wrapper.prop('onSelect')).toHaveBeenCalled();
+  });
+  it('should not contain a checkicon if it has a selected prop set to true', () => {
+    expect(wrapper.prop('selected')).toBeFalsy();
+    expect(wrapper.find(CheckIcon)).toHaveLength(0);
+  });
+  it('should contain a checkicon if it has a selected prop set to true', () => {
+    wrapper.setProps({ selected: true });
+    wrapper.update();
+    expect(wrapper.find(CheckIcon)).toHaveLength(1);
   });
 });
