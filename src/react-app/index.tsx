7905074f8be1f2aa3351f049ed5d9d6c56afab0b
Angello Obel <obel@aredah.com>
2019-03-14 16:06:47 +0300

    initial scaffold

--- src/react-app/index.tsx
+++ src/react-app/index.tsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style.css';
+import Controller from '../App/controller';
+import View from '../App/interfaces/view';
+import Db from '../db';
+import Model from '../App/interfaces/model';
+import Presenter from '../App/interfaces/presenter';
 
-class App extends Component {
+class App extends Component implements View {
+  model: Model;
+  controller: Presenter;
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.model = new Db();
+    this.controller = new Controller(this, this.model);
+  }
+
   render() {
     return (
       <div className="App">
--- src/react-app/test.tsx
+++ src/react-app/test.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
+import Model from '../db';
+import Controller from '../App/controller';
 import App from '.';
 
+jest.mock('../db');
+jest.mock('../App/controller');
+
 describe('App', () => {
-  let wrapper:ReactWrapper;
+  let wrapper: ReactWrapper;
+  let MockController;
+  let MockModel: jest.Mock;
   beforeEach(() => {
+    MockController = Controller as jest.Mocked<any>;
+    MockModel = Model as jest.Mock;
+    MockController.mockClear();
+    MockModel.mockClear();
     wrapper = mount(<App />);
   });
   it('should render without crashing', () => {
     expect(wrapper.exists()).toBeTruthy();
+  });
+  it('should initialise a model', () => {
+    expect(Model).toHaveBeenCalledTimes(1);
+    expect(Model).toHaveBeenCalledWith();
+  });
+  it('should initialise the controller with the App instance', () => {
+    expect(Controller).toHaveBeenCalledTimes(1);
+    expect(Controller).toHaveBeenCalledWith(wrapper.instance(), MockModel.mock.instances[0]);
   });
 });
