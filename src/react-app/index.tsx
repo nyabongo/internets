96632c51cb950676c9a487234ae812afcfd7b4a9
Angello Obel <obel@aredah.com>
2019-04-02 21:50:39 +0300

    fixed build errors

--- src/react-app/index.tsx
+++ src/react-app/index.tsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './style.css';
 import Controller from '../App/controller';
 import View from '../App/interfaces/view';
 import Db from '../db';
@@ -19,20 +17,7 @@ class App extends Component implements View {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       Placeholder
       </div>
     );
   }
