faee80ba2ada0e4fb0a2109683e6e2289ed92cff
Angello Obel <obel@aredah.com>
2019-05-01 10:56:04 +0300

    show provider's banner, name and  description

--- src/react-app/components/service-providers/single/index.tsx
+++ src/react-app/components/service-providers/single/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  createStyles, withStyles, Card, CardHeader, CardContent,
+} from '@material-ui/core';
+import { DBContext } from '../../../../db';
 
+const style = createStyles({
+  image: {
+    width: '100%',
+    objectFit: 'contain',
+  },
+  description: {
+    paddingTop: '0px',
+  },
+});
 interface Proptypes {
   id: string;
+  classes: any;
 }
-const ProviderPage = ({ id }: Proptypes) => (
-  <div>
-    {id}
-  </div>
-);
+const ProviderPage = ({ id, classes }: Proptypes) => {
+  const { getServiceProviderById } = useContext(DBContext);
+  const [provider, setProvider] = useState();
+  useEffect(() => {
+    getServiceProviderById(id).then((result) => {
+      setProvider(result);
+    });
+  }, [id, getServiceProviderById]);
 
-export default ProviderPage;
+  return (
+    <div>
+      <Card>
+        <img className={classes.image} role="banner" src={provider && provider.banner} alt="" />
+        <CardHeader role="heading" title={provider && provider.name} />
+        <CardContent className={classes.description} role="contentinfo">
+          {provider && provider.description}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default withStyles(style)(ProviderPage);
--- src/react-app/components/service-providers/single/test.tsx
+++ src/react-app/components/service-providers/single/test.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
+import { render, cleanup, RenderResult } from 'react-testing-library';
 import ProviderPage from '.';
+import { DBProvider } from '../../../../db';
+import { Model, ServiceProvider } from '../../../../db/interface';
+
+const provider: ServiceProvider = {
+  id: 'one',
+  name: 'Airtel Uganda',
+  description: 'Accusantium odit neque voluptates laboriosam repudiandae incidunt id voluptatum et. Aliquam provident incidunt. Tempora repellendus et. Amet enim quisquam corporis. Et ex laborum eos.',
+  website: 'https://airtel.ug',
+  banner: 'http://lorempixel.com/640/480',
+  logo: 'https://upload.wikimedia.org/wikipedia/en/1/14/Bharti_Airtel_Limited.svg',
+};
 
 describe('ProviderPage', () => {
-  let wrapper: ReactWrapper;
-  const id = 'provider-id';
-  beforeEach(() => {
-    wrapper = mount(<ProviderPage id={id} />);
+  afterAll(() => {
+    cleanup();
   });
-  it('should render without crashing', () => {
-    expect(wrapper.exists()).toBeTruthy();
+
+  let renderResult: RenderResult;
+  let db: Model;
+  beforeAll(() => {
+    db = {
+      getServiceProviderById: jest.fn(() => Promise.resolve(provider)),
+      getServiceProviders: jest.fn(),
+    };
+    renderResult = render(
+      <DBProvider value={db}>
+        <ProviderPage id={provider.id} />
+      </DBProvider>,
+    );
+  });
+  it('should call getServiceProviderById', () => {
+    expect(db.getServiceProviderById).toHaveBeenCalledWith(provider.id);
+  });
+  it('should show the provider name as the title', () => {
+    expect(renderResult.getByRole('heading').textContent).toBe(provider.name);
+  });
+  it('should show the banner', () => {
+    const banner = renderResult.getByRole('banner') as HTMLImageElement;
+    expect(banner.src).toBe(provider.banner);
+  });
+  it('should show the description as the contentinfo', () => {
+    const desc = renderResult.getByRole('contentinfo');
+    expect(desc.textContent).toBe(provider.description);
   });
 });
