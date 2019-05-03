9cf02798b67617a9cc7dd670c7265e8965267a9b
Angello Obel <obel@aredah.com>
2019-05-03 22:41:05 +0300

    shows volume and duration when they exist

--- src/react-app/components/view-widgets/detail-card/index.tsx
+++ src/react-app/components/view-widgets/detail-card/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  Card, CardHeader, CardContent, withStyles, createStyles,
+  Card, CardHeader, CardContent, withStyles, createStyles, Typography,
 } from '@material-ui/core';
-import { Thing } from '../../../../db/interface';
+import { Thing, Plan } from '../../../../db/interface';
 
 const style = createStyles({
   image: {
@@ -12,12 +12,56 @@ const style = createStyles({
   description: {
     paddingTop: '0px',
   },
+  vectors: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    '&> *': {
+      margin: '16px',
+    },
+  },
+  vectorValue: {
+    padding: '4px',
+  },
+  vectorUnit: {
+    padding: '4px',
+  },
+  vectorContainer: {
+    display: 'flex',
+  },
+  label: {
+    textAlign: 'center',
+  },
 });
 
 interface Proptypes {
-  thing: Thing;
+  thing: Thing | Plan;
   classes: any;
 }
+interface VectorProptypes {
+  value: string | number;
+  unit: string;
+  label?: string;
+  pluralChar?: string;
+  classes: any;
+}
+const Vector = withStyles(style)(({
+  value, unit, label, classes, pluralChar = '',
+}: VectorProptypes) => (
+  <div>
+    <div className={classes.vectorContainer}>
+      <Typography variant="h5" className={classes.vectorValue}>
+        {value}
+      </Typography>
+      <Typography variant="h6" className={classes.vectorUnit}>
+        {unit}
+        {value > 1 ? pluralChar : ''}
+      </Typography>
+    </div>
+    <Typography variant="caption" className={classes.label}>
+      {label && label}
+    </Typography>
+  </div>
+));
 
 
 function DetailCard({ classes, thing }: Proptypes) {
@@ -28,6 +72,23 @@ function DetailCard({ classes, thing }: Proptypes) {
       <CardContent className={classes.description} role="contentinfo">
         {thing && thing.description}
       </CardContent>
+      <div className={classes.vectors}>
+        {thing.volume && (
+          <Vector
+            value={thing.volume.value}
+            unit={thing.volume.unit}
+            label="Volume"
+          />
+        )}
+        {thing.duration && (
+          <Vector
+            value={thing.duration.value}
+            unit={thing.duration.unit}
+            pluralChar="s"
+            label="Duration"
+          />
+        )}
+      </div>
     </Card>
   );
 }
--- src/react-app/components/view-widgets/detail-card/test.tsx
+++ src/react-app/components/view-widgets/detail-card/test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, RenderResult, cleanup } from 'react-testing-library';
 import DetailCard from '.';
-import { Thing } from '../../../../db/interface';
+import { Thing, Plan } from '../../../../db/interface';
 
 const thing: Thing = {
   id: 'three',
@@ -10,6 +10,29 @@ const thing: Thing = {
   website: 'https://africell.ug',
   banner: 'http://www.accessgambia.com/information/large/africell-logo-1.jpg',
   logo: 'http://www.connectenterprise.ug/sites/default/files/africell.jpg',
+};
+
+const plan: Plan = {
+  id: '1-1-1',
+  serviceId: '1-1',
+  providerId: '1',
+  name: 'Incredible Fresh Mouse',
+  description: 'Esse mollit sit irure ut commodo ex proident.',
+  price: {
+    value: 466.00,
+    currency: 'MDL',
+    relativeMagnitude: 466.00,
+  },
+  volume: {
+    value: 784.91,
+    unit: 'MB',
+    relativeMagnitude: 784.91,
+  },
+  duration: {
+    value: 1,
+    unit: 'month',
+    relativeMagnitude: 1,
+  },
 };
 
 describe('DetailCard', () => {
@@ -30,5 +53,21 @@ describe('DetailCard', () => {
   it('should show the description as the contentinfo', () => {
     const desc = renderResult.getByRole('contentinfo');
     expect(desc.textContent).toBe(thing.description);
+  });
+  describe('with a Plan', () => {
+    beforeAll(() => {
+      cleanup();
+      renderResult = render(<DetailCard thing={plan} />);
+    });
+    it('should show the plan volume', () => {
+      expect(renderResult.queryByText(`${plan.volume.value}`)).not.toBeNull();
+      expect(renderResult.queryByText(`${plan.volume.unit}`)).not.toBeNull();
+      expect(renderResult.queryByText('Volume')).not.toBeNull();
+    });
+    it('should show the plan duration', () => {
+      expect(renderResult.queryByText(`${plan.duration.value}`)).not.toBeNull();
+      expect(renderResult.queryByText(`${plan.duration.unit}`)).not.toBeNull();
+      expect(renderResult.queryByText('Duration')).not.toBeNull();
+    });
   });
 });
