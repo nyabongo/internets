91ab74d802fcd0186408cb4c601552c40f2ec022
Angello Obel <obel@aredah.com>
2019-05-11 23:42:31 +0300

    caches script data

--- web-client/src/db/index.ts
+++ web-client/src/db/index.ts
@@ -7,13 +7,33 @@ export const DBProvider = DBContext.Provider;
 export const DBConsumer = DBContext.Consumer;
 
 export const init = (sheetId: string) => new Promise((resolve, reject) => {
+  const save = async (data: object | Promise<object>) => {
+    window.localStorage.setItem(sheetId, JSON.stringify(await data));
+  };
+
+  const load = () => {
+    const data = window.localStorage.getItem(sheetId) || '';
+    try {
+      return JSON.parse(data);
+    } catch {
+      return null;
+    }
+  };
+
+  const cached = load();
+  if (cached) {
+    resolve(cached);
+  }
+
   if (document.getElementById('gapi')) {
     reject(new Error('already initialised'));
   } else {
     const script = document.createElement('script');
     script.id = 'gapi';
     script.onload = () => {
-      resolve(getSheetData(sheetId));
+      const result = getSheetData(sheetId);
+      save(result);
+      resolve(result);
     };
     script.src = 'https://apis.google.com/js/api.js';
     document.head.appendChild(script);
